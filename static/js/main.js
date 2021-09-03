129d82194a83baf7e692dce31ace0c376ea5ce93
MutuaFranklin <franklin.mutua@student.moringaschool.com>
2021-09-03 14:28:38 +0300

    a JS script to copy

---
+++ static/js/main.js
@@ -0,0 +1,13 @@
+function copyToClipboard() {
+    /* Get the text field */
+    var copyText = document.getElementById("url");
+
+    /* Select the text field */
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); /*For mobile devices*/
+
+    /* Copy the text inside the text field */
+    document.execCommand("copy");
+
+    alert("Successfully copied Image link!");
+}
