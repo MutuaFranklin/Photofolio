61d7767013eccfbf1f3651bc2164fc9e8c06eb06
MutuaFranklin <franklin.mutua@student.moringaschool.com>
2021-09-04 16:33:05 +0300

    add location and category urls

--- gallery/urls.py
+++ gallery/urls.py
@@ -10,6 +10,7 @@ urlpatterns=[
     re_path(r'^$', views.home, name='home'),
     # re_path(r'gallery', views.gallery, name='gallery'),
     re_path(r'location/(?P<location_name>\w+)/', views.image_location, name='location'),
+    re_path(r'category/(?P<category>\w+)/', views.image_category, name='category'),
     re_path(r'^search/', views.search_results, name='search_results'),
 
     
